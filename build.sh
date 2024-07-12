70935f0b789f9862abe476c6f01948c8098a2237
Randy Palamar <randy@rnpnr.xyz>
2024-07-12 13:56:49 -0600

    add win32lib build target

--- build.sh
+++ build.sh
@@ -6,22 +6,30 @@ case "$1" in
 "win32")
 	cflags="$cflags -I./external/include"
 	ldflags="$ldflags -lgdi32 -lwinmm -L./external"
+	cc $cflags -o ogl main.c $ldflags
+	;;
+"win32lib")
+	libcflags="$cflags -fPIC -shared"
+	cc $libcflags -I'C:/Program Files/MATLAB/R2022a/extern/include' \
+		-L'C:/Program Files/MATLAB/R2022a/extern/lib/win64/microsoft' \
+		-llibmat -llibmex \
+		helpers/ogl_beamformer_lib.c -o helpers/ogl_beamformer_lib.dll
 	;;
 "lib")
-	cflags="$cflags -I"/opt/matlab/extern/include" -shared -fPIC"
-	cc $cflags helpers/ogl_beamformer_lib.c -o helpers/ogl_beamformer_lib.so
+	libcflags="$cflags -I"/opt/matlab/extern/include" -shared -fPIC"
+	cc $libcflags helpers/ogl_beamformer_lib.c -o helpers/ogl_beamformer_lib.so
 	;;
 *)
 	ldflags="$ldflags -lGL"
 
 	# Hot Reloading/Debugging
 	cflags="$cflags -D_DEBUG -Wno-unused-function"
+	#cflags="$cflags -fsanitize=address,undefined"
 
-	libcflags="$cflags -fPIC -flto -Wno-unused-function"
+	libcflags="$cflags -fPIC -Wno-unused-function"
 	libldflags="$ldflags -shared"
 
 	cc $libcflags beamformer.c -o beamformer.so $libldflags
+	cc $cflags -o ogl main.c $ldflags
 	;;
 esac
-
-cc $cflags -o ogl main.c $ldflags
