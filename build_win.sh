c5651c488a03a3e566fee59cc3501583dc4e233f
Tyler Henry <tkhenry@ualberta.ca>
2024-08-27 14:42:10 -0600

    Removing VS config

--- build_win.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-echo $(pwd)
-cflags="-march=native -g -O0 -Wall -I ./external/include"
-ldflags="-lraylib  -lgdi32 -lwinmm -lcuda_toolkit -L ./external/lib -L ../x64/Debug"
-cc $cflags -o ../x64/Debug/ogl_beamforming_cuda main.c $ldflags
-
-
