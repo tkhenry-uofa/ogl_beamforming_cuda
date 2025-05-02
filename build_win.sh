65f9af9bae40fc95eeb69bde4267538deff02d6f
Tyler Henry <tkhenry@ualberta.ca>
2025-05-02 09:15:35 -0600

    Working build script and gitignore

--- build_win.sh
+++ build_win.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-cflags="-march=native -Wall -I ./external/include"
-ldflags="-L ./external/lib . -lglfw -lraylib -lgdi32 -lwinmm -lSynchronization -lcuda_toolkit "
+cflags="-march=native -Wall -std=c11 -I ./external/include"
+ldflags="-lm -lgdi32 -lwinmm -lSynchronization -lcuda_toolkit"
 output_path="../x64"
 app_name="ogl_beamforming_cuda"
 
@@ -9,16 +9,18 @@ cc=${CC:-cc}
 
 # Check for -d flag (debug mode)
 if [ "$1" = "-d" ]; then
-    cflags="$cflags -g -O0 -D_DEBUG -Wno-unused-function -g -gcodeview"
-    c_lib_flags="$cflags -Wl,--pdb=beamformer"
+    cflags="$cflags -O0 -D_DEBUG -Wno-unused-function -fuse-ld=lld -g -gcodeview"
+    c_lib_flags="$cflags -Wl,--pdb=beamformer.pdb"
     cflags="$cflags -Wl,--pdb=${app_name}.pdb"
-    ldflags="$ldflags -fuse-ld=lld -L ../x64/Debug"
+    ldflags="-L ../x64/Debug -L. -lglfw -lraylib ${ldflags}"
     output_path="${output_path}/Debug/"
     ${cc} ${c_lib_flags} -fPIC -shared beamformer.c -o beamformer.dll ${ldflags}
     echo "Building debug OGL"
 else
     cflags="$cflags -O3"
-    ldflags="$ldflags -L ../x64/Release"
+    raylib="external/lib/libraylib.a"
+	glfw="external/lib/libglfw.a"
+	ldflags="-L ../x64/Release ${raylib} ${glfw} ${ldflags}"
     output_path="${output_path}/Release/"
     echo "Building release OGL"
 fi
